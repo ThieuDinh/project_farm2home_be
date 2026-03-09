36b04c02631c52f83003ef83f2584dfc2cc26568
ThieuDinh <dinhthieubrvt@gmail.com>
2026-03-09 22:02:53 +0700

    add log feature who do it

--- Controllers/userapi.cs
+++ Controllers/userapi.cs
@@ -44,8 +44,9 @@ namespace usersApi.Controllers
         [HttpPost]
         public IActionResult CreateUser([FromBody] User newUser)
         {
+            var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Không xác định";
             _context.Users.Add(newUser);
-            _context.AuditLogs.Add(new AuditLog { Action = "THÊM", Details = $"Thêm user mới: {newUser.name}", CreatedAt = DateTime.UtcNow.AddHours(7) });
+            _context.AuditLogs.Add(new AuditLog { Action = "THÊM", Details = $"Thêm user mới: {newUser.name}", CreatedAt = DateTime.UtcNow.AddHours(7),IpAddress = clientIp });
             _context.SaveChanges(); // Lưu vĩnh viễn vào DB thật
             return Ok(newUser);
         }
@@ -54,11 +55,12 @@ namespace usersApi.Controllers
         [HttpPut("{id}")]
         public IActionResult UpdateUser(int id, [FromBody] User updatedUser)
         {
+             var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Không xác định";
             var user = _context.Users.FirstOrDefault(u => u.id == id);
             if (user == null) return NotFound();
             string oldName = user.name;
             user.name = updatedUser.name;
-            _context.AuditLogs.Add(new AuditLog { Action = "SỬA", Details = $"Đổi tên ID {id} từ '{oldName}' thành '{updatedUser.name}'", CreatedAt = DateTime.UtcNow.AddHours(7) });
+            _context.AuditLogs.Add(new AuditLog { Action = "SỬA", Details = $"Đổi tên ID {id} từ '{oldName}' thành '{updatedUser.name}'", CreatedAt = DateTime.UtcNow.AddHours(7) ,IpAddress = clientIp});
             _context.SaveChanges(); // Cập nhật vào DB
             return Ok(user);
         }
@@ -67,11 +69,12 @@ namespace usersApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteUser(int id)
         {
+             var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Không xác định";
             var user = _context.Users.FirstOrDefault(u => u.id == id);
             if (user == null) return NotFound();
 
             _context.Users.Remove(user);
-            _context.AuditLogs.Add(new AuditLog { Action = "XÓA", Details = $"Xóa user ID {id} (Tên: {user.name})", CreatedAt = DateTime.UtcNow.AddHours(7) });
+            _context.AuditLogs.Add(new AuditLog { Action = "XÓA", Details = $"Xóa user ID {id} (Tên: {user.name})", CreatedAt = DateTime.UtcNow.AddHours(7) ,IpAddress = clientIp});
             _context.SaveChanges(); // Xóa khỏi DB
             return Ok(new { message = "Xóa thành công!" });
         }
