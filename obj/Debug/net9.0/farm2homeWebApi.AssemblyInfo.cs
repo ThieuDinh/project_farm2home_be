57ec0f8d526afd720d4e6ddc0a496be0dfc606ff
ThieuDinh <dinhthieubrvt@gmail.com>
2026-03-25 10:34:11 +0700

    chore: change host

--- obj/Debug/net9.0/farm2homeWebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/farm2homeWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("farm2homeWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9658c06c5039a26fcf159b2f9dc276a18ab502b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d389de6b5346a339dc79cebb9362f4a7fa6b8f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("farm2homeWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("farm2homeWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
